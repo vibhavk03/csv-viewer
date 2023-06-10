dc579b52871d58e6f6fa7d45b16410f25ff0e809
vibhavk03 <vibhavkashyap3@gmail.com>
2023-06-10 10:43:56 +0530

    rendering files from db on home page

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,7 +1,10 @@
+const CSV = require('../models/CSV');
+
 module.exports = {
   home: async function (req, res) {
+    const files = await CSV.find({}).sort('-createdAt');
     res.render('home', {
-      title: 'CSV Viewer',
+      files,
     });
   },
 };
